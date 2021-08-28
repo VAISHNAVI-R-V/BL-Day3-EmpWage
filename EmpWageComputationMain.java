d8b28afdb0178bf077934c9f1ccbddaa727d8d6f
VAISHNAVI-R-V <83284920+VAISHNAVI-R-V@users.noreply.github.com>
2021-08-28 23:10:02 +0530

    ADDED: Employee Wage of Multiple Companies

---
+++ EmpWageComputationMain.java
@@ -0,0 +1,13 @@
+package com.bl.d3;
+
+public class EmpWageComputationMain {
+    public static void main(String[] args){
+        System.out.println("Welcome Employee Wage Computation!!");
+
+        EmpWageComputationImpl empWageComputation = new EmpWageComputationImpl();
+        empWageComputation.addCompany("TATA",20,2,50);
+        empWageComputation.addCompany("RELIANCE", 10, 4,100);
+        empWageComputation.computEmpWage();
+    }
+
+}
