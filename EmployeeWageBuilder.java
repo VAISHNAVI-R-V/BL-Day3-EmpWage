d7aae69affc4f04797a87cd0fe18e39c1c6aa64a
VAISHNAVI-R-V <83284920+VAISHNAVI-R-V@users.noreply.github.com>
2021-08-28 17:27:15 +0530

    ADDED: Total Wage For Each Company

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,32 +1,94 @@
 public class EmployeeWageBuilder {
-    public static final int IS_PART_TIME = 1;
-    public static final int IS_FULL_TIME = 2;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int NO_OF_WORKING_DAYS = 2;
-    public static final int MAX_HRS_IN_MONTH = 10;
+    // declaring instance variables
+    private static final int ABSENT = 0;
+    private static final int FULL_TIME = 1;
+    private static final int PART_TIME = 2;
+    private final String companyName;
+    private final int wagePerHour;
+    private final int numberOfWorkingDays;
+    private final int maxWorkingHours;
 
-    public static void main(String[] args) {
-        // Variables
-        int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-        // Computation
-        while (totalEmpHrs <=  MAX_HRS_IN_MONTH &&
-                totalWorkingDays < NO_OF_WORKING_DAYS ) {
-            totalWorkingDays ++;
+    // creating constructor
+    private EmployeeWageBuilder(String companyName, int wagePerHour,
+                                int numberOfWorkingDays, int maxWorkingHours) {
+        this.companyName = companyName;
+        this.wagePerHour = wagePerHour;
+        this.numberOfWorkingDays = numberOfWorkingDays;
+        this.maxWorkingHours = maxWorkingHours;
+    }
+
+    // Checks employee attendance and calculates Salary
+    private static void employeeAttendanceChecker(String company, int hourlyWage, int totalDays, int maxHours) {
+        int absentCount = 0;
+        int fullTimeCount = 0;
+        int partTimeCount = 0;
+        int totalWorkingHours = 0;
+        int totalEmpWage = 0;
+
+        // checks attendance and calculates salary for month or total working days
+        for (int i = 1; i <= totalDays; i++) {
+
+            int empHrs = 0;
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             switch (empCheck) {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
+                case ABSENT -> {
+                    absentCount++;
                     empHrs = 0;
+                }
+
+                case FULL_TIME -> {
+                    fullTimeCount++;
+                    empHrs = 8;
+                }
+
+                case PART_TIME -> {
+                    partTimeCount++;
+                    empHrs = 4;
+                }
             }
-            totalEmpHrs += empHrs;
-            System.out.println("Days : " + totalWorkingDays + ", Emp Hrs : " + empHrs );
+            totalWorkingHours += empHrs;
+
+            if (totalWorkingHours >= maxHours) {
+                totalWorkingHours = maxHours;
+                break;
+            }
+
+            int wageForTheDay = wageCalculator(hourlyWage, empHrs);
+            totalEmpWage += wageForTheDay;
         }
-        int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-        System.out.println("Total Emp Wage : " + totalEmpWage);
+        displayInfo(company, absentCount, fullTimeCount, partTimeCount, totalWorkingHours, totalEmpWage);
+    }
+
+    // displays all information on console
+    public static void displayInfo(String cName, int aCount, int fTCount,
+                                   int pTCount, int tHours, int totalEmpWage) {
+
+        System.out.println("Following gives the Employee Wage Details for the company : " + cName);
+        System.out.println("Employee Absent : " + aCount + " days");
+        System.out.println("Employee Full Time : " + fTCount + " days");
+        System.out.println("Employee Part Time : " + pTCount + " days");
+        System.out.println("Total Working Hours : " + tHours + " hours");
+        System.out.println("Employee total wage is : " + totalEmpWage + " Rs /-");
+    }
+    // returns wage for the day
+    public static int wageCalculator(int hourlyWage, int empHrs) {
+        return hourlyWage * empHrs;
+    }
+
+    //main method
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program !!! ");
+
+        // creating object for Dmart company and storing their details regarding employee wage
+        EmployeeWageBuilder dmart = new EmployeeWageBuilder("D-MART", 20, 20, 100);
+
+        // creating object for Reliance Retail company and storing their details regarding employee wage
+        EmployeeWageBuilder reliance = new EmployeeWageBuilder("Reliance Retail", 10, 30, 200);
+
+        employeeAttendanceChecker(dmart.companyName, dmart.wagePerHour,
+                dmart.numberOfWorkingDays, dmart.maxWorkingHours);
+        System.out.println();
+        employeeAttendanceChecker(reliance.companyName, reliance.wagePerHour,
+                reliance.numberOfWorkingDays, reliance.maxWorkingHours);
     }
 }
